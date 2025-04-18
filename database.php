e8d20f2e8b7e002d99e5b923424adfb202dd7439
Seckene Sako <seckene_sako@hotmail.com>
2025-04-19 00:11:26 +0200

    commit4

--- database.php
+++ database.php
@@ -21,3 +21,25 @@ try {
 } catch (PDOException $e) {
     throw new PDOException($e->getMessage(), (int)$e->getCode()); // En cas d'erreur, on affiche le message
 }
+
+
+
+
+// Si formulaire soumis
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nom = $_POST['nom'];
+    $cin = $_POST['cin'];
+    $date_naissance = $_POST['date_naissance'];
+    $telephone = $_POST['telephone'];
+    $email = $_POST['email'] ?? null; // Email est facultatif
+    $ville = $_POST['ville'];
+    $sexe = $_POST['sexe'];
+
+    // Requête SQL pour insérer les données
+    $sql = "INSERT INTO utilisateurs (nom_complet, numero_cin, date_naissance, telephone, email, ville, sexe)
+            VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$nom, $cin, $date_naissance, $telephone, $email, $ville, $sexe]);
+
+    $message = "✅ Inscription enregistrée avec succès !";
+}
