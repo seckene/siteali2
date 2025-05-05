c3c0e78fde5e15ff8e74bb0bdb7647d513feb25e
Seckene Sako <seckene_sako@hotmail.com>
2025-05-05 22:56:41 +0200

    commit number81

---
+++ contact.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dar as'salammm</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Anton&family=Ballet:opsz@16..72&family=Boldonse&family=Bungee+Tint&family=DM+Serif+Text:ital@0;1&family=Dancing+Script:wght@400..700&family=Danfo&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lobster&family=M+PLUS+Rounded+1c&family=Matemasie&family=Oswald:wght@200..700&family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&family=Playwrite+CO+Guides&family=Playwrite+IN:wght@100..400&family=Playwrite+NL+Guides&family=Playwrite+VN:wght@100..400&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sevillana&display=swap"
+        rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=Ballet:opsz@16..72&family=Boldonse&family=Bungee+Tint&family=DM+Serif+Text:ital@0;1&family=Dancing+Script:wght@400..700&family=Danfo&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lobster&family=M+PLUS+Rounded+1c&family=Matemasie&family=Oswald:wght@200..700&family=Permanent+Marker&family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&family=Playwrite+CO+Guides&family=Playwrite+IN:wght@100..400&family=Playwrite+NL+Guides&family=Playwrite+VN:wght@100..400&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Sevillana&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav class="navb">
+            <ul class="logetnom">
+                <img src="android-icon-96x96.png">
+                <li class="titre">Dar as'salam</li>
+            </ul>
+
+            <ul class="reste">
+                <div class="fab">
+                    <button class="croix">X</button>
+                    <li id="ays"><a href="index.php"> Accueil</a></li>
+                    <li id="ays"><a href="omra.php">Omra 2025</a></li>
+                    <li id="ays"><a href="inscription.php">Inscription Hadj 2025</a></li>
+                    <li id="ays"><a href="administratif.php">Administratif</a></li>
+                   
+                    <li id="ays"><a href=""> Contact</a></li>
+
+                </div>
+            </ul>
+            <svg xmlns="http://www.w3.org/2000/svg" height="32" width="28" viewBox="0 0 448 512"
+                class="boss"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                <path fill="#030303"
+                    d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+            </svg>
+        </nav>
+    </header>
+
+    <div class="ctrl">
+        <div class="gauche"></div>
+  
+        <div class="droit">
+       
+                      
+  <div class="salif2">
+    <form method="post">
+
+    <label for="nom" > Nom</label>
+      <input type="text" id="nom"  name="nom" required >
+
+      <label for="email">Email </label>
+      <input type="email" id="nom" name="email">
+
+      <label for="ville">Message </label>
+      <input type="text" id="nom2" name="ville" required>
+
+      <button type="submit">S'inscrire</button>
+
+
+    </form>
+    
+  </div>
+  </div>
+    </div>
