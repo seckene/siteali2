31fe99bb3bba3885772c1b3c1057b580bc3c7656
Seckene Sako <seckene_sako@hotmail.com>
2025-06-13 21:57:44 +0200

    commit number125

--- ex.js
+++ ex.js
@@ -2,18 +2,48 @@ let reste =document.querySelector(".reste")
 let burger=document.querySelector(".boss")
 let  fab=document.querySelector(".fab")
 let titre=document.querySelector(".titre44")
-console.log(titre)
+let navb=document.querySelector("nav")
+
+let croix =document.querySelector(".croix")
+
+let Omra20255=document.querySelector(".Omra20255")
+let goos2=document.querySelector(".goos2")
+
+let passtext = document.querySelector(".passtext");
+console.log(navb)
+
+console.log(goos2)
+console.log(passtext)
+
+// window.addEventListener("scroll",()=>{
+//     if(window.scrollY<100){
+
+// navb.style.background="rgba(20, 20, 20, 0.322)"
+// navb.style.border = "2px solid rgba(255, 255, 255, 0.05)";
+// navb.style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.5)";
+// navb.style.backdropFilter = "blur(2.5px)";
+// navb.style.borderBottomLeftRadius = "1rem";
+// navb.style.borderBottomRightRadius = "1rem";
+// navb.style.zIndex = "1";
+// navb.style.width = "100%";
+// navb.style.position = "fixed";
+
+
+//     }
+    
+// })
+
 
 burger.addEventListener("click",()=>{
    
     reste.style.display= "block" ;
+    
     burger.style.display="none"
     if (window.innerWidth<600) {
         burger.style.display="none"
      }
 })
 
-let croix =document.querySelector(".croix")
 
 croix.addEventListener("click",()=>{
   burger.style.display="flex"
@@ -21,11 +51,6 @@ croix.addEventListener("click",()=>{
    
 })
 
-let Omra20255=document.querySelector(".Omra20255")
-let goos2=document.querySelector(".goos2")
-console.log(goos2)
-let passtext = document.querySelector(".passtext");
-console.log(passtext)
 goos2.addEventListener("mouseover", (e) => {
     passtext.style. display= "flex";
 
